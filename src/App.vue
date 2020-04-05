cd74b39cc21bbde0c7c0e657f8b02852a1a81445
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-05 18:06:37 +0000

    Deploy Factory-Rise/factory-rise.github.io to github.com/Factory-Rise/factory-rise.github.io.git:master

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <p>welcome</p>
+    <img src="./assets/logo.png">
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'app',
+    components: {
+      
+    }
+  }
+</script>
