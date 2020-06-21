aa3d3d2c552091de14e9f3d0a45426f1adbbbb3b
DoctorVWA <doctorvwa43@gmail.com>
2020-06-21 17:11:13 -0300

    Site base & starting tests

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,20 @@
     <div class="header">
       <img src="./assets/logo.png">
     </div>
+
+    <div class="dynamicPage">
+      <component v-bind:is="currentPage" class="currentPage"></component>
+    </div>
   </div>
 </template>
 
 <script>
   export default {
-    name: 'app',
+    name: '.app',
+    data: {
+      currentPage: "home",
+      pages: ["home", "state", "about"]
+    },
     components: {}
   }
 </script>
